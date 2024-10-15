6326e9dd3abf7da7d75abe5a42bcd9fc8f411fe7
kadeyasa <kadeyasa@gmail.com>
2024-10-15 16:14:30 +0800

    update data

--- app/Controllers/Tabungan.php
+++ app/Controllers/Tabungan.php
@@ -207,8 +207,9 @@ class Tabungan extends BaseController
 
     function rekaptabungan(){
         $model = NEW Mutasitabunganmodel();
-        $model->select("s.*, n.no_rekening,n.nama,n.alamat")
+        $model->select("s.*, n.no_rekening,n.nama,n.alamat,b.saldo")
             ->from("tb_mutasi_tabungan as s")  // Alias for tb_saldo_tabungan
+            ->join('tb_saldo_tabungan b','n.id=b.id_nasabah')
             ->join('tbnasabah_tabungan as n', 'n.id = s.id_nasabah','left');  // Alias for tbnasabah_tabungan
 
         // Check if a keyword is set and not empty
